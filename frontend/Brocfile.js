f0ea3679298219ceee4092e6c94c9a6e5015f617
Jean Lucas <jeanleonino@gmail.com>
2015-02-07 15:52:31 -0200

    Importing Bootstrap js

--- frontend/Brocfile.js
+++ frontend/Brocfile.js
@@ -2,7 +2,11 @@
 
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-var app = new EmberApp();
+var app = new EmberApp({
+  'ember-cli-bootstrap': {
+    'importBootstrapJS': true
+  }
+});
 
 // Use `app.import` to add additional libraries to the generated
 // output files.
